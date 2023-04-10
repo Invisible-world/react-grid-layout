e4705a681f381371e35fc45c0013b0a5c4df96d4
invisible-world <sameerpokharel29@gmail.com>
2023-04-11 01:51:14 +0545

    change yarn  to npm for pre deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import Canvas from "./component/Canvas";
 import Layout from "./component/Layout";
 import { GridProvider } from "./hooks/useGridLayout";
-import "react-grid-layout/css/styles.css";
-import "react-resizable/css/styles.css";
 
 function App() {
 	return (
